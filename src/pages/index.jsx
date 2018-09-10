efdd0faec7d183640ba4044a2f029626c6e51176
Cihan Bebek <cihan.m.bebek@gmail.com>
2018-09-10 19:11:29 +0300

    add little front page content

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,8 @@ const IndexPage = () => (
     <div>
       <p>Luuppi on perustettu vuonna 1969 matematiikan ja tietojenkäsittelyn oppiaineiden ainejärjestöksi. Tätä 50-vuotista taivalta Luuppi juhlistaa 23.2.2019 järjestettävillä vuosijuhlillaan.</p>
       <p>Luupin historiaan mahtuu paljon tapahtumia: …</p>
-      <p>50-vuotisjuhlia vietetään ennennäkemättömällä tavalla. Tapahtumaa riittää koko vuosijuhlaviikon, jonka ohjelma löytyy TÄÄLTÄ. Itse pääjuhla järjestetään 23.2. Paja Kongressissa aivan Torni-hotellin juuressa. Lippu juhliin maksaa 100 euroa jäsenille (120 euroa ei-jäsenille) ja se sisältää pääsyn pääjuhlaan, pääjuhlan jatkoille, pääjuhlan jatkojen...</p>
+      <p>50-vuotisjuhlia vietetään ennennäkemättömällä tavalla. Tapahtumaa riittää koko vuosijuhlaviikon, jonka ohjelma löytyy TÄÄLTÄ. Itse pääjuhla järjestetään 23.2. Paja Kongressissa aivan Torni-hotellin juuressa. Lippu juhliin maksaa 100 euroa jäsenille (120 euroa ei-jäsenille) ja se sisältää pääsyn pääjuhlaan, pääjuhlan jatkoille, pääjuhlan jatkojen jatkoille, seuraavan päivän sillikselle sekä silliksen jatkoille.</p>
+      <p>Ilmoittautuminen vuosijuhlille tapahtuu Luupin verkkosivuilta sekä jäsenille että jäsenten aveceille. Kutsuvieraille on lähetetty oma ilmoittautumislinkki. Yleinen ilmoittautuminen kaikille luuppilaisille alkaa marraskuussa.</p>
     </div>
   </Layout>
 )
