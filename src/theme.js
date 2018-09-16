a613701cba6938c6ac61dbc567754efd5af57ada
naftis <pyry.rouvila@gmail.com>
2018-09-16 16:12:00 +0300

    add progress bar to give visual cue to user that new page is loading

--- src/theme.js
+++ src/theme.js
@@ -16,6 +16,9 @@ export const sizes = {
   desktop: 1050,
 }
 
+/*
+ * https://web.archive.org/web/20180914172900/https://www.styled-components.com/docs/advanced#media-templates
+ */
 export const media = Object.keys(sizes).reduce((acc, label) => {
   acc[label] = (...args) => css`
     @media (max-width: ${sizes[label] / 16}em) {
