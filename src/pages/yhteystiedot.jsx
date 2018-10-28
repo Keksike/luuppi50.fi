4a24a7644315d498b48408cc736d498f5cad536b
Cihan Bebek <cihan.m.bebek@gmail.com>
2018-10-28 16:13:10 +0000

    fix styles on contacts page

--- src/pages/yhteystiedot.jsx
+++ src/pages/yhteystiedot.jsx
@@ -1,57 +1,91 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import styled from 'styled-components'
 
+import { media } from '../theme'
 import Content from '../components/Content'
-import { Pair, Text } from '../components/Pair'
+
+const ContentRow = styled.div`
+  display: flex;
+
+  ${media.smallMobile`
+    display: block;
+  `};
+`
+
+const Half = styled.div`
+  flex: 1;
+  text-align: center;
+`
+
+const Text = styled.div`
+  padding: 2rem 0;
+  min-width: 60%;
+  margin: 0;
+  ${media.smallMobile`
+    max-width: none;
+    padding: 1rem 0;
+  `};
+`
 
 const ContactPage = () => (
   <Content>
     <Helmet>
       <title>Yhteystiedot</title>
     </Helmet>
-
     <h3>Yhteystiedot</h3>
-
-    <Pair>
-      <Text>
-        <h4>Tuomas Himmanen</h4>
-        <p>Vuosijuhlakoordinaattori</p>
-        <p>vuosijuhlat@luuppi.fi</p>
-      </Text>
-
-      <Text>
-        <h4>Jonna Paksunen</h4>
-        <p>Pääjuhlavastaava</p>
-      </Text>
-    </Pair>
-    <Pair>
-      <Text>
-        <h4>Cihan Bebek</h4>
-        <p>Jatkovastaava</p>
-      </Text>
-
-      <Text>
-        <h4>Niko Ruotsalainen</h4>
-        <p>Sillisvastaava</p>
-      </Text>
-    </Pair>
-    <Pair>
-      <Text>
-        <h4>Akseli Koskela</h4>
-        <p>Cocktailtilaisuus- ja tiedotusvastaava</p>
-      </Text>
-
-      <Text>
-        <h4>Loviisa Hurme</h4>
-        <p>Yritysvastaava</p>
-        <p>loviisa.hurme@luuppi.fi</p>
-      </Text>
-    </Pair>
-
-    <Text>
-      <h4>Lydia Isokallio</h4>
-      <p>Grafiikkavastaava</p>
-    </Text>
+    <ContentRow>
+      <Half>
+        <Text>
+          <h4>Tuomas Himmanen</h4>
+          <p>Vuosijuhlakoordinaattori</p>
+          <p>vuosijuhlat@luuppi.fi</p>
+        </Text>
+      </Half>
+      <Half>
+        <Text>
+          <h4>Jonna Paksunen</h4>
+          <p>Pääjuhlavastaava</p>
+        </Text>
+      </Half>
+    </ContentRow>
+    <ContentRow>
+      <Half>
+        <Text>
+          <h4>Cihan Bebek</h4>
+          <p>Jatkovastaava</p>
+        </Text>
+      </Half>
+      <Half>
+        <Text>
+          <h4>Niko Ruotsalainen</h4>
+          <p>Sillisvastaava</p>
+        </Text>
+      </Half>
+    </ContentRow>
+    <ContentRow>
+      <Half>
+        <Text>
+          <h4>Akseli Koskela</h4>
+          <p>Cocktailtilaisuus- ja tiedotusvastaava</p>
+        </Text>
+      </Half>
+      <Half>
+        <Text>
+          <h4>Loviisa Hurme</h4>
+          <p>Yritysvastaava</p>
+          <p>loviisa.hurme@luuppi.fi</p>
+        </Text>
+      </Half>
+    </ContentRow>
+    <ContentRow>
+      <Half>
+        <Text>
+          <h4>Lydia Isokallio</h4>
+          <p>Grafiikkavastaava</p>
+        </Text>
+      </Half>
+    </ContentRow>
   </Content>
 )
 
